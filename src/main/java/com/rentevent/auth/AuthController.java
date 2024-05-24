04226dff88b81c9734fa7a8718e8df0b617e165f
kevoaac <130163890+kevoaac@users.noreply.github.com>
2024-05-24 17:10:29 -0500

    feat: CORS

--- src/main/java/com/rentevent/auth/AuthController.java
+++ src/main/java/com/rentevent/auth/AuthController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+//@CrossOrigin(origins = {"http://localhost:5173"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:5173"})
 public class AuthController {
 
     private final AuthService authService;
