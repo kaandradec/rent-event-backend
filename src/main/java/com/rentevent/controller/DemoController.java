92cb5fd08ec144d12fef850ac6f92f0fafca5e73
kaandradec <130163890+kaandradec@users.noreply.github.com>
2024-06-14 21:29:03 -0500

    build: añadir API KEY

---
+++ src/main/java/com/rentevent/controller/DemoController.java
@@ -0,0 +1,43 @@
+package com.rentevent.controller;
+
+import com.rentevent.model.servicio.ServicioDTO;
+import com.rentevent.model.servicio.ServicioRequest;
+import com.rentevent.model.servicio.ServicioResponse;
+import com.rentevent.service.ServicioService;
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/demo")
+@RequiredArgsConstructor
+// Lombok genera un constructor con los atributos marcados como final, hace que no sea necesario inyectar las dependencias con @Autowired
+//@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:3000"}) // Permite el acceso a los recursos desde el servidor local de React: Vite -> http://localhost:5173
+public class DemoController {
+    private final ServicioService servicioService;
+
+    private String apiKey;
+    @GetMapping(value = "/apikey")
+    public ResponseEntity<String> getApiKey() {
+        Dotenv dotenv = Dotenv.load();
+        apiKey = dotenv.get("API_KEY");
+
+        return ResponseEntity.ok("API-KEY desde archivo .env -> " + apiKey);
+    }
+
+    @GetMapping(value = "{codigo}")
+    public ResponseEntity<ServicioDTO> getServicio(@PathVariable String codigo) {
+        ServicioDTO servicioDTO = servicioService.getService(codigo);
+        if (servicioDTO == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(servicioDTO);
+    }
+
+    @PutMapping()
+    public ResponseEntity<ServicioResponse> updateService(@RequestBody ServicioRequest servicioRequest) {
+        return ResponseEntity.ok(servicioService.updateService(servicioRequest));
+    }
+}
